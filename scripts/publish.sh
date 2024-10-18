f8484600ee776b87226c78ce722a79d6e9d0c5e0
yl <2201441955@qq.com>
2024-10-18 15:15:09 +0800

    Upgrade mojo to latest nightly

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,8 @@
 
 # ignore errors because we want to ignore duplicate packages
 for file in $CONDA_BLD_PATH/**/*.conda; do
-    magic run rattler-build upload prefix -c "mojo-community" "$file" || true
+    echo "Uploading $file..."
+    magic run rattler-build upload prefix -c "mojo-libs-f0cii-nightly" "$file" --api-key=$PREFIX_DEV_API_KEY || true
 done
 
 rm $CONDA_BLD_PATH/**/*.conda
